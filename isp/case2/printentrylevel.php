04998179bee1e340364790630062edf9cef7ef8f
farhan-venn <ankarsa1287@gmail.com>
2024-04-24 21:06:36 +0700

    commit: Menambahkan file printentrylevel

---
+++ isp/case2/printentrylevel.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'fiturCetak.php';
+
+class PrinterEntryLevel implements FiturCetak {
+    public function cetakKertas(): void {
+        echo "Printer Entry Level: Mencetak kertas...\n";
+    }
+}
