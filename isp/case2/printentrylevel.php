8e925e85278572fde3b79ce3d1a4c5a6af9d9beb
farhan-venn <65814996+farhan-venn@users.noreply.github.com>
2024-04-24 21:15:13 +0700

    Delete isp/case2/printentrylevel.php

--- isp/case2/printentrylevel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'fiturCetak.php';
-
-class PrinterEntryLevel implements FiturCetak {
-    public function cetakKertas(): void {
-        echo "Printer Entry Level: Mencetak kertas...\n";
-    }
-}
