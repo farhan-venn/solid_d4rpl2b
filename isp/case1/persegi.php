32235fb1157d7fb6e1942f5c7133a53591aaeb16
farhan-venn <ankarsa1287@gmail.com>
2024-04-24 20:54:36 +0700

    commit: Menambahkan file persegi

---
+++ isp/case1/persegi.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'Shape2Dimension.php';
+
+class Persegi implements Shape2Dimension {
+    public function calculateArea(): void {
+        echo "Calculating area of a square...\n";
+    }
+}
